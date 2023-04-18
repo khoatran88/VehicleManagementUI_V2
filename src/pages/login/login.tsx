b4974cb9c28619ae5c186cc3ac8e81700c657b01
mphduong <105898895+mphduong@users.noreply.github.com>
2023-04-18 11:24:56 +0700

    message

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { useAuthContext } from 'src/context'
 import { useSignIn } from 'src/hooks/auth'
+import logo1 from 'src/asset/images/logo-merc.jpg'
 
 export default function Login() {
   const [user, setUser] = useState('')
   const [pwd, setPwd] = useState('')
-  const [errMsg, setErrMsg] = useState('')
+  // const [errMsg, setErrMsg] = useState('')
   const { setTokens } = useAuthContext()
 
   useEffect(() => {
-    setErrMsg('')
+    // setErrMsg('')
   }, [user, pwd])
 
   const { mutate: handleSignIn } = useSignIn()
@@ -30,34 +31,70 @@ export default function Login() {
   }
 
   return (
-    <div className="login-wrapper">
-      <section>
-        <p className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">
+    <div className="grid h-screen w-full grid-cols-1 sm:grid-cols-2">
+      <div className="hidden bg-sky-700 sm:block">
+        <div className="h-full w-full bg-sky-700 object-cover">aaa</div>
+        {/* <p className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">
           {errMsg}
-        </p>
-        <h1>Sign In</h1>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <div>Username:</div>
-          <input
-            type="text"
-            id="username"
-            autoComplete="off"
-            onChange={(e) => setUser(e.target.value)}
-            value={user}
-            required
-          />
+        </p> */}
+      </div>
+      <div className="flex flex-col justify-center">
+        <form
+          className="mx-auto w-full max-w-[400px]"
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <div className="flex w-full items-center justify-center">
+            <img src={logo1} alt="LOGO" />
+          </div>
+          <h1 className="py-7 text-center text-[20px] font-bold dark:text-blue-800">
+            Wellcome To TNG Vehicle Management
+          </h1>
+          <div className="pb-5">
+            <label htmlFor="username" className="flex flex-col">
+              Username
+              <input
+                className="mt-2 rounded-lg border-x border-blue-800 p-2 focus:border-blue-500 focus:outline-none"
+                type="text"
+                id="username"
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+              />
+            </label>
+          </div>
 
-          <div>Password:</div>
-          <input
-            type="password"
-            id="password"
-            onChange={(e) => setPwd(e.target.value)}
-            value={pwd}
-            required
-          />
-          <button type="submit">Sign In</button>
+          <div className="pb-5">
+            <label htmlFor="password" className="flex flex-col">
+              Password
+              <input
+                className="mt-2 rounded-lg border-blue-800 p-2 focus:border-blue-500 focus:outline-none"
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+              />
+            </label>
+          </div>
+
+          <div className="flex justify-between pb-5">
+            <div className="remember-me">
+              <p className="flex items-center">
+                <input className="mr-2" type="checkbox" /> Remember me
+              </p>
+            </div>
+            <h4>Recover password</h4>
+          </div>
+
+          <button
+            className="w-full rounded-lg bg-blue-500 p-3 shadow-lg"
+            type="submit"
+          >
+            Sign In
+          </button>
         </form>
-      </section>
+      </div>
     </div>
   )
 }
