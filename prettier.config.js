086a1f2d168b0f39fbf3062a0681b93e70a16e89
pcnguyen <pcnguyen@tma.com.vn>
2023-04-19 16:09:09 +0700

    add Mui-ui and remove tailwind

--- prettier.config.js
+++ prettier.config.js
@@ -7,5 +7,4 @@ module.exports = {
   importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-  plugins: [require('prettier-plugin-tailwindcss')],
 }
