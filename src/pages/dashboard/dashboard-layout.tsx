aa8dd4bf177e176394961881c038e39293eb15a3
mphduong <105898895+mphduong@users.noreply.github.com>
2023-04-21 11:42:46 +0700

    message

--- src/pages/dashboard/dashboard-layout.tsx
+++ src/pages/dashboard/dashboard-layout.tsx
@@ -1,71 +1,15 @@
-import { Suspense, useState } from 'react'
+import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 import { SmallSpinner } from 'src/components'
-import GetVehicle from 'src/api/routes/vehicle'
-import { ModalLayout } from '../modal-layout'
+import { TableLayout } from '../vehicle-manager/table-layout'
 
 export default function DashboardLayout() {
-  const [modalShow, setModalShow] = useState(false)
-  const data = GetVehicle()
   return (
     <div className="grow px-8 py-5 sm:px-20">
       <Suspense fallback={<SmallSpinner />}>
         <Outlet />
       </Suspense>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">STT</th>
-            <th scope="col">Plate Number</th>
-            <th scope="col">Brand</th>
-            <th scope="col">Model</th>
-            <th scope="col">Engine Number</th>
-            <th scope="col">Chassis Number</th>
-            <th scope="col">Color</th>
-            <th scope="col">Type</th>
-            <th scope="col">Production Year</th>
-            <th scope="col">Life Time Limit To</th>
-            <th scope="col">Purchased Date</th>
-            <th scope="col">Last Inspection Date</th>
-            <th scope="col">Last Updated Date</th>
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>{data.plateNumber}</td>
-            <td>{data.brand}</td>
-            <td>{data.model}</td>
-            <td>{data.engineNumber}</td>
-            <td>{data.chassisNumber}</td>
-            <td>{data.color}</td>
-            <td>{data.type}</td>
-            <td>{data.productionYear}</td>
-            <td>{data.lifetimeLimitTo}</td>
-            <td>{data.purchasedDate}</td>
-            <td>{data.lastInspectionDate}</td>
-            <td>{data.lastUpdatedDate}</td>
-            <td>
-              <button
-                type="button"
-                className="btn btn-primary"
-                onClick={() => setModalShow(true)}
-              >
-                Edit Vehicle
-              </button>
-              <button type="button" className="btn btn-primary">
-                Delete
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <ModalLayout
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        data={data}
-      />
+      <TableLayout />
     </div>
   )
 }
