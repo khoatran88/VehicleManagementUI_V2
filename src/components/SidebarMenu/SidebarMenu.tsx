bb654f59bf9458f7e53df46a2523e5ee3d11d319
pcnguyen <pcnguyen@tma.com.vn>
2023-05-15 14:58:28 +0700

    add select language

--- src/components/SidebarMenu/SidebarMenu.tsx
+++ src/components/SidebarMenu/SidebarMenu.tsx
@@ -9,24 +9,25 @@ import { useTranslation } from 'react-i18next'
 import i18next from 'i18next'
 import { useMemo } from 'react'
 
-
 const SidebarMenu = () => {
   const { t } = useTranslation('common')
   const languages = [
     {
       code: 'vn',
       name: t('common.vn'),
-      flag: vnFlag
+      flag: vnFlag,
     },
     {
       code: 'en',
       name: t('common.en'),
-      flag: ukFlag
-    }
+      flag: ukFlag,
+    },
   ]
 
-  const renderLangBtn = languages.find(item => item.code === i18next.language)
-  
+  const renderLangBtn = languages.find(
+    (item) => item.code === i18next.resolvedLanguage
+  )
+
   return (
     <div className="sidebar min-vh-100 d-flex flex-column">
       <div className="sidebar-header d-flex align-items-center justify-content-center mb-3 py-2">
@@ -49,30 +50,51 @@ const SidebarMenu = () => {
       </div>
       <div className="sidebar-footer">
         <div className="dropdown">
-        {renderLangBtn && <button className="btn py-2 w-100 d-flex" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <div className='button-content'>
-            <div className='button-icon'>
-              <img src={renderLangBtn.flag} alt="lang" />
-            </div>
-            <div className="button-text ps-3">
-              <div className='text'>
-                {t('common.language')}
+          {renderLangBtn && (
+            <button
+              className="btn py-2 w-100 d-flex"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <div className="button-content">
+                <div className="button-icon">
+                  <img src={renderLangBtn.flag} alt="lang" />
+                </div>
+                <div className="button-text ps-3">
+                  <div className="text">{t('common.language')}</div>
+                  <div className="selected-lang">{renderLangBtn.name}</div>
+                </div>
               </div>
-              <div className='selected-lang'>
-                {renderLangBtn.name}
-              </div>
-            </div>
-          </div>
-        </button>}
-          <ul className="dropdown-menu p-0 dropdown-menu-end ">
-            <li><span className="dropdown-item-text selectLng">{t('common.selectLng')}</span></li>
-            {languages.map(({code, name, flag}) => (
+            </button>
+          )}
+          <ul className="dropdown-menu p-0 dropdown-menu-end">
+            <li>
+              <span className="dropdown-item-text selectLng">
+                {t('common.selectLng')}
+              </span>
+            </li>
+            {languages.map(({ code, name, flag }) => (
               <li key={code}>
-                <a className="dropdown-item d-flex py-3" onClick={() => {i18next.changeLanguage(code)}}>
-                <img src={flag} alt="flag" className='ms-2 me-3'/>
-                  <div>
-                    {name}
+                <a
+                  className={
+                    'dropdown-item d-flex py-3 justify-content-between align-items-center ' +
+                    (i18next.resolvedLanguage !== code
+                      ? 'unselect'
+                      : 'selected')
+                  }
+                  onClick={() => {
+                    i18next.changeLanguage(code)
+                  }}
+                >
+                  <div className="d-flex">
+                    <img src={flag} alt="flag" className="ms-2 me-3" />
+                    <div>{name}</div>
                   </div>
+                  <i
+                    className="bi bi-check fs-4"
+                    hidden={i18next.resolvedLanguage !== code ? true : false}
+                  ></i>
                 </a>
               </li>
             ))}
